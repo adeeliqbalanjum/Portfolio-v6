27b31f6e79308cfc84d050ee50d979faab9902c1
adeeliqbalanjum <bitm-f20-090@superior.edu.pk>
2026-06-21 16:36:21 +0500

    Use one avatar header logo everywhere

--- app/components/SiteHeader.tsx
+++ app/components/SiteHeader.tsx
@@ -1,9 +1,13 @@
 import Link from 'next/link';
 
+const portraitDataUrl = 'https://avatars.githubusercontent.com/u/178131381?v=4';
+
 export function SiteHeader() {
   return (
     <nav className="nav site-header" aria-label="Primary navigation">
-      <Link className="nav-logo" href="/" aria-label="Muhammad Adeel Iqbal home">MA</Link>
+      <Link className="nav-logo" href="/" aria-label="Muhammad Adeel Iqbal home">
+        <img src={portraitDataUrl} alt="Adeel" />
+      </Link>
       <Link href="/">Home</Link>
       <Link href="/portfolio">Portfolio</Link>
       <Link href="/#about">About</Link>
